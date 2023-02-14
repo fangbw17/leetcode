d665402f046e7fd89e201c5ed3fbdff6724eaabb
fangbw17 <bowen.fang17@gmail.com>
2023-02-14 21:09:09 +0800

    最大二叉树

--- src/654.最大二叉树/README.md
+++ src/654.最大二叉树/README.md
@@ -41,7 +41,25 @@ nums 中的所有整数 互不相同
 
 ## 题解
 ```javascript
+var constructMaximumBinaryTree = function (nums) {
+  return construct(nums, 0, nums.length - 1)
+};
+const construct = (nums, left, right) => {
+  if (left > right) {
+    return null
+  }
+  let max = left
+  for (let i = left; i <= right; i++) {
+    if (nums[max] < nums[i]) {
+      max = i
+    }
+  }
 
+  const node = new TreeNode(nums[max])
+  node.left = construct(nums, left, max - 1)
+  node.right = construct(nums, max + 1, right)
+  return node;
+}
 ```
 
 
--- src/654.最大二叉树/index.js
+++ src/654.最大二叉树/index.js
@@ -1,33 +1,31 @@
 var constructMaximumBinaryTree = function (nums) {
-  if (nums.length <= 1) return nums;
-
-  // 最大值
-  let maxNum = nums[0];
-  // 左子树
-  let lTree = [];
-  // 右子树
-  let rTree = [];
-  nums.forEach((num) => {
-    if (num > maxNum) maxNum = num;
-  });
-  const maxIdx = nums.findIndex((num) => num == maxNum);
-
-  nums.forEach((num, index) => {
-    if (maxIdx > index) {
-      lTree.push(num);
-    } else if (maxIdx < index) {
-      rTree.push(num);
-    }
-  });
-
-  let lValue = [null];
-  let rValue = [null];
-  if (lTree.length > 0) lValue = constructMaximumBinaryTree(lTree);
-  if (rTree.length > 0) rValue = constructMaximumBinaryTree(rTree);
-
-  return [maxNum, ...lValue, ...rValue];
+  return construct(nums, 0, nums.length - 1)
 };
 
-// console.log(constructMaximumBinaryTree([2,30,4,8,10,3,7]));
-console.log(constructMaximumBinaryTree([3, 4,  6, 0, 5]));
+const construct = (nums, left, right) => {
+  if (left > right) {
+    return null
+  }
+  let max = left
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[max] < nums[i]) {
+      max = i
+    }
+  }
 
+  const node = new TreeNode(nums[max])
+  node.left = construct(nums, left, max - 1)
+  node.right = construct(nums, max + 1, right)
+  return node;
+}
+
+
+// console.log(constructMaximumBinaryTree([2,30,4,8,10,3,7]));
+console.log(constructMaximumBinaryTree([3,2,1,6,0,5]));
+
+
+function TreeNode(val, left, right) {
+  this.val = (val===undefined ? 0 : val)
+  this.left = (left===undefined ? null : left)
+  this.right = (right===undefined ? null : right)
+} 
